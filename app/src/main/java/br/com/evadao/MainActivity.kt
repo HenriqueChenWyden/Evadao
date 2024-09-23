111e226a1d30a5fd44080bb91891bf3b2e91ba71
Henrique Chen <129029894+HenriqueChenWyden@users.noreply.github.com>
2024-09-23 10:48:27 -0300

    Initial

--- app/src/main/java/br/com/evadao/MainActivity.kt
+++ app/src/main/java/br/com/evadao/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,15 +21,24 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EvadãoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    //TopBar & BottomBar
                 }
             }
         }
     }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun AppBarTop(
+    title: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable () -> Unit = {}
+){
+
 }
 
 @Composable
