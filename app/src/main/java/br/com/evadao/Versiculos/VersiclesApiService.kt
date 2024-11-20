bd7528fa425f7078c8673a45bee6b3d7f1e30e08
HenriqueChen <57077907+HenriqueChen@users.noreply.github.com>
2024-11-20 20:09:26 -0300

    Tradução feita e appbar retornado

--- app/src/main/java/br/com/evadao/Versiculos/VersiclesApiService.kt
+++ app/src/main/java/br/com/evadao/Versiculos/VersiclesApiService.kt
@@ -4,22 +4,23 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 import retrofit2.Call
 
 class VersiclesApiService {
-    // Definir a interface do serviço com parâmetro dinâmico
     interface VerseApiService {
-        @GET("{verse}")  // Usando um parâmetro dinâmico para o versículo
-        fun getRandomVerse(@Path("verse") verse: String): Call<VerseResponse>
+        @GET("{verse}")
+        fun getRandomVerse(
+            @Path("verse") verse: String,
+            @Query("translation") translation: String = "almeida"
+        ): Call<VerseResponse>
     }
 
-    // Definir o modelo de dados que vai representar a resposta
     data class VerseResponse(
         val reference: String,
         val text: String
     )
 
-    // Instanciar o Retrofit
     object ApiClient {
         private const val BASE_URL = "https://bible-api.com/"
 
