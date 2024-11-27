692c8d9223dffd6fab37bd32f1923ff323aa5992
Henrique Chen <129029894+henriquechenwyden@users.noreply.github.com>
2024-11-27 10:37:42 -0300

    New Page: Oração Selection Author @HenriqueChenWyden
    Signed-off-by: Henrique Chen <129029894+HenriqueChenWyden@users.noreply.github.com>

--- app/src/main/java/br/com/evadao/Oracao/OracaoSelectionScreen.kt
+++ app/src/main/java/br/com/evadao/Oracao/OracaoSelectionScreen.kt
@@ -1,12 +1,14 @@
-package br.com.evadao.Home
+package br.com.evadao.Oracao
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,35 +25,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import br.com.evadao.AppBarTop
 import br.com.evadao.BottomAppBar
-import br.com.evadao.Oracao.OracaoSelection
+import br.com.evadao.Home.Home
+import br.com.evadao.Home.SquareButton
 import br.com.evadao.R
 import br.com.evadao.Versiculos.VersiculosScreen
 import br.com.evadao.ui.theme.EvadãoTheme
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun HomeScreen() {
+fun NavigationSelection() {
     val navController = rememberNavController()
 
     Scaffold(
         topBar = {
-            AppBarTop() // Calling the TopAppBar in the Scaffold
+            AppBarTop()
         },
         bottomBar = {
-            BottomAppBar(navController = navController) // Passing NavController to BottomAppBar
+            BottomAppBar(navController = navController)
         },
         content = {
             NavHost(navController = navController, startDestination = "home_screen") {
@@ -61,8 +62,8 @@ fun HomeScreen() {
                 composable("versiculos_screen") {
                     VersiculosScreen().Aplicacao()
                 }
-                composable("oracao_screen"){
-                    OracaoSelection()
+                composable("oracao_screen") {
+                    // OracaoScreen()
                 }
             }
         }
@@ -70,112 +71,102 @@ fun HomeScreen() {
 }
 
 @Composable
-fun Home(navController: NavController) {
+fun OracaoSelection() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color(0x54D8D8D8)),
+            .background(color = Color.White),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Spacer(modifier = Modifier.height(70.dp))
+        Spacer(modifier = Modifier.height(16.dp))
         Text(
-            text = "Conectados pela \n\n         Fé",
+            text = "Escolha a sua oração",
             color = Color.Black,
             fontWeight = FontWeight.Normal,
             fontStyle = FontStyle.Normal,
-            fontSize = 55.sp,
-            fontFamily = customFontFamily,
+            fontFamily = FontFamily.SansSerif,
+            fontSize = 20.sp,
             modifier = Modifier.padding(30.dp)
         )
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            RectangleButton(
+                buttonText = "",
+                label = "Pai-Nosso",
+                colorResourceId = R.color.marrom_ciclista,
             ) {
-                SquareButton(
-                    buttonText = "",
-                    label = "Versículos",
-                    colorResourceId = R.color.marrom_ciclista,
-                    imageResId = R.drawable.cruz
-                ) {
-                    navController.navigate("versiculos_screen")
-                }
-                SquareButton(
-                    buttonText = "",
-                    label = "História",
-                    colorResourceId = R.color.marrom_ciclista,
-                    imageResId = R.drawable.livro
-                ) { /* Button 2 action */ }
+                // navController.navigate("versiculos_screen")
             }
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                SquareButton(
-                    buttonText = "",
-                    label = "Orações",
-                    colorResourceId = R.color.marrom_ciclista,
-                    imageResId = R.drawable.vela
-                ) {
-                    navController.navigate("oracao_screen")
-                }
-                SquareButton(
-                    buttonText = "",
-                    label = "Sobre",
-                    colorResourceId = R.color.marrom_ciclista,
-                    imageResId = R.drawable.info
-                ) { /* Button 4 action */ }
-            }
+            RectangleButton(
+                buttonText = "",
+                label = "Ave-Maria",
+                colorResourceId = R.color.marrom_ciclista,
+            ) { /* Button 2 action */ }
         }
+        Spacer(modifier = Modifier.height(8.dp))
+        Column(
+            modifier = Modifier.padding(16.dp),
+
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            RectangleButton(
+                buttonText = "",
+                label = "Sinal da Cruz",
+                colorResourceId = R.color.marrom_ciclista,
+            ) { /* Button 3 action */ }
+            RectangleButton(
+                buttonText = "",
+                label = "Credo",
+                colorResourceId = R.color.marrom_ciclista,
+            ) { /* Button 4 action */ }
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        RectangleButton(
+            buttonText = "",
+            label = "Glória",
+            colorResourceId = R.color.marrom_ciclista,
+        ) { /* Button 5 action */ }
     }
 }
 
 @Composable
-fun SquareButton(
+fun RectangleButton(
     buttonText: String,
     label: String,
     colorResourceId: Int,
-    imageResId: Int,
     onClick: () -> Unit
 ) {
     val color = colorResource(id = colorResourceId)
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        modifier = Modifier.padding(4.dp)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
     ) {
         Button(
             onClick = onClick,
             modifier = Modifier
-                .size(150.dp),
-            shape = RoundedCornerShape(8.dp),
+                .fillMaxWidth()
+                .height(70.dp),
+            shape = RoundedCornerShape(2.dp),
             colors = ButtonDefaults.buttonColors(containerColor = color)
         ) {
-            Icon(
-                painter = painterResource(id = imageResId),
-                contentDescription = label,
-                modifier = Modifier.size(80.dp),
-                tint = Color.White
-            )
+            // Empty content for the button
         }
-        Spacer(modifier = Modifier.height(3.dp))
-        Text(text = label, fontSize = 20.sp)
+        Text(text = label, fontSize = 20.sp, color = Color.White)
     }
 }
-
-val customFontFamily = FontFamily(
-    Font(R.font.italiano_regular)
-)
 
 @Preview(showBackground = true)
 @Composable
 fun Aplicacao() {
     EvadãoTheme {
         Surface {
-            HomeScreen()
+            OracaoSelection()
         }
     }
 }
